3a527b925d890fd9da4416f3e7f2e42dc73fb773
GeorgeRadev <GeorgeRadev@gmail.com>
2013-07-29 11:40:04 +0000

    adding test case for date handler

--- test/cx/TestHandlers.java
+++ test/cx/TestHandlers.java
@@ -10,8 +10,15 @@ public class TestHandlers extends TestCase {
 			Context cx = new Context();
 			cx.addHandler(new DateHandler());
 			cx.evaluate((new Parser(
-					"var date = newDate('2013-07-08','yyyy-MM-dd'); var datestr = formatDate(date,'dd/MM/yyyy');")).parse());
-			assertEquals("08/07/2013", cx.get("datestr"));
+					"var date = newDate(); date.year=2013; date.month=07;date.day=08 ;var datestr = formatDate(date,'yyyy-MM-dd');")).parse());
+			assertEquals("2013-07-08", cx.get("datestr"));
+		}
+		{
+			Context cx = new Context();
+			cx.addHandler(new DateHandler());
+			cx.evaluate((new Parser(
+					"var date = newDate('08/07/2013','dd/MM/yyyy'); var datestr = formatDate(date,'yyyy-MM-dd');")).parse());
+			assertEquals("2013-07-08", cx.get("datestr"));
 		}
 	}
 
