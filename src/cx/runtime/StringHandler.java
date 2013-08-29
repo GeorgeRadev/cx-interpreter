abc219d97d3c9f2cdf74710cf70d57e8ea6659d8
GeorgeRadev <GeorgeRadev@gmail.com>
2013-08-29 11:58:20 +0000

    adding StringHandler

---
+++ src/cx/runtime/StringHandler.java
@@ -0,0 +1,235 @@
+package cx.runtime;
+
+import cx.Context;
+import cx.ast.Visitor;
+
+public class StringHandler implements ObjectHandler {
+	public static enum StringMethod {
+		length, trim, substring, replace, indexOf, lastIndexOf, startsWith, endsWith, toLowerCase, toUpperCase;
+
+		public static StringMethod parse(final String str) {
+			String guess = null;
+			final int l = str.length();
+			StringMethod method = null;
+
+			switch (l) {
+				case 4:
+					if ('t' == str.charAt(0)) {
+						guess = "trim";
+						method = trim;
+					}
+					break;
+				case 6:
+					if ('l' == str.charAt(0)) {
+						guess = "length";
+						method = length;
+					}
+					break;
+				case 7:
+					switch (str.charAt(0)) {
+						case 'i':
+							guess = "indexOf";
+							method = indexOf;
+							break;
+						case 'r':
+							guess = "replace";
+							method = replace;
+							break;
+					}
+					break;
+				case 8:
+					if ('e' == str.charAt(0)) {
+						guess = "endsWith";
+						method = endsWith;
+					}
+					break;
+				case 9:
+					if ('s' == str.charAt(0)) {
+						guess = "substring";
+						method = substring;
+					}
+					break;
+				case 10:
+					if ('s' == str.charAt(0)) {
+						guess = "startsWith";
+						method = startsWith;
+					}
+					break;
+				case 11:
+					switch (str.charAt(2)) {
+						case 'L':
+							guess = "toLowerCase";
+							method = toLowerCase;
+							break;
+						case 'U':
+							guess = "toUpperCase";
+							method = toUpperCase;
+							break;
+						case 's':
+							guess = "lastIndexOf";
+							method = lastIndexOf;
+							break;
+					}
+					break;
+			}
+			if ((guess != null) && !guess.equals(str)) {
+				return null;
+			}
+			return method;
+		}
+	}
+
+	private static class StringCall {
+		final String string;
+		final StringMethod method;
+
+		StringCall(String string, StringMethod method) {
+			this.string = string;
+			this.method = method;
+		}
+	}
+
+	public void init(Visitor cx) {}
+
+	public boolean accept(Object object) {
+		return object instanceof String || object instanceof StringCall;
+	}
+
+	public void set(Object object, String variable, Object value) {}
+
+	public Object get(Object thiz, String variable) {
+		StringMethod method = StringMethod.parse(variable);
+		if (method != null) {
+			return new StringCall((String) thiz, method);
+		}
+		return null;
+	}
+
+	public Object call(Object object, Object[] args) {
+		if (!(object instanceof StringCall)) {
+			return null;
+		}
+
+		StringCall stringCall = (StringCall) object;
+		if (args.length == 2) {
+			if (args[0] != null && args[1] != null) {
+				switch (stringCall.method) {
+					case replace: {
+						String str1 = args[0].toString();
+						String str2 = args[1].toString();
+						if (str1.length() == 1 && str2.length() == 1) {
+							return stringCall.string.replace(str1.charAt(0), str2.charAt(0));
+						} else {
+							return stringCall.string.replace(str1, str2);
+						}
+					}
+					case substring: {
+						int i1 = Context.toLong(args[0]).intValue();
+						int i2 = Context.toLong(args[1]).intValue();
+						String str = stringCall.string;
+						int l = str.length();
+						if (i2 > l) {
+							i2 = l;
+						}
+						if (i1 < 0) {
+							i1 = 0;
+						}
+						if (i1 > i2) {
+							return "";
+						}
+						return str.substring(i1, i2);
+					}
+					case indexOf: {
+						String index = args[0].toString();
+						int i1 = Context.toLong(args[1]).intValue();
+						String str = stringCall.string;
+						int l = str.length();
+						if (i1 > l) {
+							i1 = l;
+						}
+						if (i1 < 0) {
+							i1 = 0;
+						}
+						if (index.length() == 1) {
+							return str.indexOf(index.charAt(0), i1);
+						} else {
+							return str.indexOf(index, i1);
+						}
+					}
+					default:
+						break;
+				}
+			}
+		} else if (args.length == 1) {
+			if (args[0] != null) {
+				switch (stringCall.method) {
+					case indexOf: {
+						String index = args[0].toString();
+						String str = stringCall.string;
+						if (index.length() == 1) {
+							return str.indexOf(index.charAt(0));
+						} else {
+							return str.indexOf(index);
+						}
+					}
+					case endsWith: {
+						String str = args[0].toString();
+						return stringCall.string.endsWith(str);
+					}
+					case substring: {
+						int i = Context.toLong(args[0]).intValue();
+						String str = stringCall.string;
+						int l = str.length();
+						if (i < 0) {
+							i = 0;
+						}
+						if (i > l) {
+							return "";
+						}
+						return str.substring(i);
+					}
+					case startsWith: {
+						String str = args[0].toString();
+						return stringCall.string.startsWith(str);
+					}
+					case lastIndexOf: {
+						String index = args[0].toString();
+						String str = stringCall.string;
+						if (index.length() == 1) {
+							return str.lastIndexOf(index.charAt(0));
+						} else {
+							return str.lastIndexOf(index);
+						}
+					}
+					default:
+						break;
+				}
+			}
+		}
+		switch (stringCall.method) {
+			case trim:
+				return stringCall.string.trim();
+			case length:
+				return stringCall.string.length();
+			case toLowerCase:
+				return stringCall.string.toLowerCase();
+			case toUpperCase:
+				return stringCall.string.toUpperCase();
+			default:
+				break;
+		}
+		return null;
+	}
+
+	public boolean acceptStaticCall(String method, Object[] args) {
+		return "toString".equals(method);
+	}
+
+	public Object staticCall(String method, Object[] args) {
+		StringBuilder buffer = new StringBuilder();
+		for (Object obj : args) {
+			buffer.append(obj);
+		}
+		return buffer.toString();
+	}
+}
--- test/cx/TestHandlers.java
+++ test/cx/TestHandlers.java
@@ -3,8 +3,58 @@ package cx;
 import junit.framework.TestCase;
 import cx.runtime.DateHandler;
 import cx.runtime.MathHandler;
+import cx.runtime.StringHandler;
 
 public class TestHandlers extends TestCase {
+	public void testStringHandler() {
+		{
+			Context cx = new Context();
+			cx.addHandler(new StringHandler());
+			cx.evaluate((new Parser("str = toString(5);")).parse());
+			assertEquals("5", cx.get("str").toString());
+
+			cx.evaluate((new Parser("str = ' trim '; str = str.trim();")).parse());
+			assertEquals("trim", cx.get("str").toString());
+			cx.evaluate((new Parser("str = ('te' + 'st ').trim();")).parse());
+			assertEquals("test", cx.get("str"));
+
+			cx.evaluate((new Parser("str = 'test'.length();")).parse());
+			assertEquals("test".length(), cx.get("str"));
+			cx.evaluate((new Parser("str = 'smallCammelCase'.toLowerCase();")).parse());
+			assertEquals("smallCammelCase".toLowerCase(), cx.get("str"));
+			cx.evaluate((new Parser("str = 'smallCammelCase'.toUpperCase();")).parse());
+			assertEquals("smallCammelCase".toUpperCase(), cx.get("str"));
+
+			cx.evaluate((new Parser("str = 'ccaabb'.replace('aa','cc');")).parse());
+			assertEquals("ccaabb".replace("aa", "cc"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.replace('a','c');")).parse());
+			assertEquals("ccaabb".replace('a', 'c'), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.substring(1,4);")).parse());
+			assertEquals("ccaabb".substring(1, 4), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.substring(4);")).parse());
+			assertEquals("ccaabb".substring(4), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.indexOf('aa',2);")).parse());
+			assertEquals("ccaabb".indexOf("aa", 2), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.indexOf('a',2);")).parse());
+			assertEquals("ccaabb".indexOf('a', 2), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.indexOf('aa');")).parse());
+			assertEquals("ccaabb".indexOf("aa"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.indexOf('a');")).parse());
+			assertEquals("ccaabb".indexOf('a'), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.endsWith('aa');")).parse());
+			assertEquals("ccaabb".endsWith("aa"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.endsWith('bb');")).parse());
+			assertEquals("ccaabb".endsWith("bb"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.startsWith('aa');")).parse());
+			assertEquals("ccaabb".startsWith("aa"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.startsWith('cc');")).parse());
+			assertEquals("ccaabb".startsWith("cc"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.lastIndexOf('aa');")).parse());
+			assertEquals("ccaabb".lastIndexOf("aa"), cx.get("str"));
+			cx.evaluate((new Parser("str = 'ccaabb'.lastIndexOf('a');")).parse());
+			assertEquals("ccaabb".lastIndexOf('a'), cx.get("str"));
+		}
+	}
 
 	public void testMathHandler() {
 		{
